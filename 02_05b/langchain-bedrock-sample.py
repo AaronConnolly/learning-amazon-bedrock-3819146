a02da71ec851fb1aadf43a7aa7e5b5cd0c2e0606
Aaron Connolly <124158487+AaronConnolly@users.noreply.github.com>
2025-06-23 10:57:13 +0000

    LangChain & Bedrock Sample created

--- 02_05b/langchain-bedrock-sample.py
+++ 02_05b/langchain-bedrock-sample.py
@@ -1,11 +1,36 @@
 #Imports
+import boto3
+from langchain_aws import ChatBedrock
+
 
 #Create the bedrock client
+boto3_client = boto3.client('bedrock-runtime')
 
 #setting model inference parameters
+inference_modifier = {
+    "temperature": 0.5,
+    "max_tokens": 1000,
+    "top_p": 1,
+}
 
 #Create the llm
+llm = ChatBedrock(
+    model_id="anthropic.claude-3-haiku-20240307-v1:0",
+    client=boto3_client,
+    model_kwargs=inference_modifier
+)
 
-#Generate the response
+# Format the prompt as a message list
+messages = [
+    {
+        "role": "user",
+        "content": "Write an email from Mark, Hiring Manager, welcoming a new employee 'John Doe' to the company on his first day."
+    }
+]
 
-#Display the result
+# Generate the response
+response = llm.invoke(messages)
+
+# Display the result
+print(response.content)
+
